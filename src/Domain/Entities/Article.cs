912c190d7e2c1aa5e984f031bd3abafed5c86119
Daniel Nakolah <nakolahdaniel1@gmail.com>
2022-01-31 02:45:26 +0300

    Create a Articles and add styling

--- src/Domain/Entities/Article.cs
+++ src/Domain/Entities/Article.cs
@@ -3,11 +3,13 @@ namespace Domain.Entities;
 public sealed record Article : AuditableEntity, IHasDomainEvent
 {
     public Guid Id { get; init; }
+    public string Title { get; set; } = default!;
+    public string Content { get; set; } = default!;
+    public string Excerpt { get; set; } = default!;
     public DateTime ReleaseDate { get; set; }
 
     public Author Author { get; set; } = default!;
     public ICollection<Like> Likes { get; set; } = default!;
-
 
     public List<DomainEvent> DomainEvents { get; init; } = default!;
 }
