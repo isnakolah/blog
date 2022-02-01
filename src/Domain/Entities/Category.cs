9a73c02ba874a4768082cc593d3f4c3386570a2d
Daniel Nakolah <nakolahdaniel1@gmail.com>
2022-02-01 14:59:43 +0300

    Add a many to may relationship for Articles and Category

--- src/Domain/Entities/Category.cs
+++ src/Domain/Entities/Category.cs
@@ -6,5 +6,7 @@ public record Category : AuditableEntity, IHasDomainEvent
     public string Name { get; set; } = default!;
     public string Slug => string.Join("-", Name.Split(" ").Select(s => s.ToLower()));
 
+    public ICollection<Article>? Articles { get; set; }
+    
     public List<DomainEvent> DomainEvents { get; init; } = default!;
 }
