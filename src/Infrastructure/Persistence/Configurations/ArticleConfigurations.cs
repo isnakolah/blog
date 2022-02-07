1b786a6f9de081503e65e562fe9681346d9674b5
Daniel Nakolah <nakolahdaniel1@gmail.com>
2022-02-07 06:25:01 +0300

    Add index to the Article Title

--- src/Infrastructure/Persistence/Configurations/ArticleConfigurations.cs
+++ src/Infrastructure/Persistence/Configurations/ArticleConfigurations.cs
@@ -8,9 +8,13 @@ public class ArticleConfigurations : IEntityTypeConfiguration<Article>
 {
     public void Configure(EntityTypeBuilder<Article> builder)
     {
-        builder.Ignore(x => x.DomainEvents);
+        builder.Ignore(a => a.DomainEvents);
 
-        builder.HasMany(x => x.Categories)
+        builder.HasMany(a => a.Categories)
             .WithMany(c => c.Articles);
+
+        builder.HasIndex(a => a.Title)
+            .IsUnique()
+            .HasFilter(null);
     }
 }
