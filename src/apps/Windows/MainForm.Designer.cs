a40dd94e02118b542929366dd8606eb50f5b985a
Daniel Nakolah <nakolahdaniel1@gmail.com>
2022-02-09 06:24:35 +0300

    Change MainForm name

--- src/apps/Windows/MainForm.Designer.cs
+++ src/apps/Windows/MainForm.Designer.cs
@@ -49,7 +49,7 @@ partial class MainForm
         this.components = new System.ComponentModel.Container();
         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
         this.ClientSize = new System.Drawing.Size(800, 450);
-        this.Text = "Form1";
+        this.Text = "ZeBlog";
     }
 
     #endregion
