bcc2436740871bd43b9e1baf444a578887461ee0
Daniel Nakolah <nakolahdaniel1@gmail.com>
2022-02-12 19:52:14 +0300

    # 10 Remove slugs from the domain objects

--- src/common/Domain/Entities/Article.cs
+++ src/common/Domain/Entities/Article.cs
@@ -6,7 +6,6 @@ public sealed record Article : AuditableEntity, IHasDomainEvent
 {
     public Guid Id { get; init; }
     public string Title { get; set; } = default!;
-    public string Slug => string.Join("-", Title.Split(" ").Select(s => s.ToLower()));
     public string Content { get; set; } = default!;
     public string Excerpt { get; set; } = default!;
     public Uri? FeaturedImageUri { get; set; }
--- src/common/Domain/Entities/Category.cs
+++ src/common/Domain/Entities/Category.cs
@@ -4,8 +4,6 @@ public record Category : AuditableEntity, IHasDomainEvent
 {
     public Guid Id { get; init; }
     public string Name { get; set; } = default!;
-    public string Slug => string.Join("-", Name.Split(" ").Select(s => s.ToLower()));
-
     public ICollection<Article>? Articles { get; set; }
 
     public List<DomainEvent> DomainEvents { get; init; } = new();
