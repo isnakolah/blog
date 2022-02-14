3e356407c9f0913cbce320d88b3455641870ad3e
Daniel Nakolah <nakolahdaniel1@gmail.com>
2022-02-14 05:19:33 +0300

    #30 Create a Performance middleware

---
+++ src/common/Application/Common/Behaviours/PerformanceBehaviour.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Common.Application.Common.Behaviours;
+
+internal sealed class PerformanceBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+{
+    private readonly Stopwatch _timer;
+    private readonly ILogger<TRequest> _logger;
+
+    public PerformanceBehaviour(ILogger<TRequest> logger)
+    {
+        (_timer, _logger) = (new Stopwatch(), logger);
+    }
+
+    public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+    {
+        _timer.Start();
+
+        var response = await next();
+
+        _timer.Stop();
+
+        var elapsedMilliseconds = _timer.ElapsedMilliseconds;
+
+        if (elapsedMilliseconds > 500)
+            _logger.LogWarning("EPharmacy Long Running Request: {Name} ({ElapsedMilliseconds} milliseconds) {@Request}",typeof(TRequest).Name, elapsedMilliseconds, request);
+
+        return response;
+    }
+}
