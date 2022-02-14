330b0e0ea02f2b5b841da753f9ab5e79bbdf0887
Daniel Nakolah <nakolahdaniel1@gmail.com>
2022-02-14 03:57:40 +0300

    #24 Add SlugExtensions with CreateSlug and RemakeStringFromSlug extension methods on string

---
+++ src/common/Application/Common/SlugExtensions.cs
@@ -0,0 +1,18 @@
+namespace Common.Application.Common;
+
+internal static class SlugExtensions
+{
+    public static string CreateSlug(this string str)
+    {
+        var slug = string.Join("-", str.ToLower().Split(" ", StringSplitOptions.TrimEntries));
+
+        return slug;
+    }
+
+    public static string RemakeStringFromSlug(this string slug)
+    {
+        var str = string.Join(" ", slug.Split("-"));
+
+        return str;
+    }
+}
